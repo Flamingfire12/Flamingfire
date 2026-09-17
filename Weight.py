03ac39d67bf3e804b1b6cd57dae27f2fad228a71
Flamingfire12 <edemsewornu087@gmail.com>
2026-09-17 09:32:21 +0000

    code is not runing need review and fixes

---
+++ Weight.py
@@ -0,0 +1,5 @@
+full_name = input("What is your full name :")
+Weight = input("what is your weigh in pounds :")
+kg =0.453592 * (Weight)
+print("So your name is " + full_name+ " And your weight is " + str(kg)+ "kg")
+
